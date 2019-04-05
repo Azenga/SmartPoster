9aba82a2ff8d5e9c9696f341c82dd39ca355dfba
Azenga Kevin <toomuch573@gmail.com>
2019-04-05 10:33:19 +0300

    Did some public profile stuff and editing profiling layouts also linking to blog post for a logged in user

--- app/src/main/java/com/shadow/smartposter/fragments/publics/EditProfileFragment.java
+++ app/src/main/java/com/shadow/smartposter/fragments/publics/EditProfileFragment.java
@@ -2,18 +2,14 @@ package com.shadow.smartposter.fragments.publics;
 
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 
-import com.shadow.smartposter.PublicDashboardActivity;
 import com.shadow.smartposter.R;
 
-public class ProfileFragment extends Fragment {
+public class EditProfileFragment extends Fragment {
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -22,12 +18,12 @@ public class ProfileFragment extends Fragment {
     private String mParam2;
 
 
-    public ProfileFragment() {
+    public EditProfileFragment() {
         // Required empty public constructor
     }
 
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
+    public static EditProfileFragment newInstance(String param1, String param2) {
+        EditProfileFragment fragment = new EditProfileFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -47,16 +43,7 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return inflater.inflate(R.layout.fragment_edit_profile, container, false);
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        ImageButton editProfileIB = view.findViewById(R.id.edit_profile_bi);
-
-        editProfileIB.setOnClickListener(v -> {
-            ((PublicDashboardActivity) getActivity()).getSupportActionBar().setTitle("Edit Profile");
-            ((PublicDashboardActivity) getActivity()).switchFragment(new EditProfileFragment());
-        });
-    }
 }
